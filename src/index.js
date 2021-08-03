e64d66e8d26a18ab3d8daa5471ef9fd6e6128c7d
Edson J Vargas <edson.jhair@yahoo.com>
2021-08-03 17:30:15 -0400

    Initial commit: Se prepara el entorno de trabajo
    Se organiza la estructira del proyecto, se renombran archivos por defecto, se instalan el paquete node-sass y bootstrap, se agrega info al archivo .gitignore y se eliminan archivos innecesarios. Se agregan recursos de diseño y guía de estilo.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/scss/index.scss';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 

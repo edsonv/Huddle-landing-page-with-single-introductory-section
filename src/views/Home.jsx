8f8fabfbec162e592d099a7b4e02103808881336
Edson J Vargas <edson.jhair@yahoo.com>
2021-08-08 14:41:40 -0400

    Se corrigen estilos para dispositivos mas pequeños

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,16 +6,16 @@ const Home = () => {
   return (
     <div className="container-fluid px-4">
       <nav className="brand-bar row justify-content-center">
-        <div className="imageContainer col-10 col-md-9 col-lg-11 text-left">
+        <div className="imageContainer col-12 col-sm-10 col-md-9 col-lg-11 text-left">
           <img className="image float-start" src={ Logo } alt="Huddle Logo" />
         </div>
       </nav>
       <header className="hero-section">
         <div className="row justify-content-center justify-content-lg-center gx-5">
-          <div className="imageContainer col-10 col-md-9 col-lg-6">
+          <div className="imageContainer col-12 col-sm-10 col-md-9 col-lg-6">
             <img className="image img-fluid" src={ Mockup } alt="" />
           </div>
-          <div className="copyContainer col-10 col-lg-5 text-lg-start">
+          <div className="copyContainer col-12 col-sm-10 col-lg-5 text-lg-start">
             <h1 className="title text-center text-lg-start mx-auto mx-lg-0">Build the community your fans will love</h1>
             <p className="lead text-center text-lg-start mx-auto mx-lg-0">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
             <button className="button .-register">Register</button>
@@ -37,7 +37,7 @@ const Home = () => {
         </div>
       </header>
       <footer className="footer row justify-content-center">
-        <div className="col-10">
+        <div className="col-12 col-sm-10">
           <p className="attribution">Challenge by <a href="https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/edsonv">Edson J Vargas</a>.</p>
         </div>
       </footer>
